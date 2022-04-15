b089d2796441cdbbbcb46dd677145c0f3599dd75
Alireza Fatemi <alirezafa@rocketmail.com>
2022-04-16 01:01:00 +0300

    🏗️ Integrate ray tracer and area light code.

--- assignment4/src/base/AreaLight.cpp
+++ assignment4/src/base/AreaLight.cpp
@@ -1,32 +1,72 @@
-
 #include "AreaLight.hpp"
 
+#include <iostream>
 
 namespace FW {
-
 
 void AreaLight::draw(const Mat4f& worldToCamera, const Mat4f& projection) {
     glUseProgram(0);
     glMatrixMode(GL_PROJECTION);
     glLoadMatrixf((float*)&projection);
     glMatrixMode(GL_MODELVIEW);
-    Mat4f S = Mat4f::scale(Vec3f(m_size,1));
-    Mat4f M = worldToCamera *m_xform * S;
+    Mat4f S = Mat4f::scale(Vec3f(m_size, 1));
+    Mat4f M = worldToCamera * m_xform * S;
     glLoadMatrixf((float*)&M);
     glBegin(GL_TRIANGLES);
-    glColor3fv( &m_E.x );
-    glVertex3f(1,1,0); glVertex3f(1,-1,0); glVertex3f( -1,-1,0 );
-    glVertex3f(1,1,0); glVertex3f( -1,-1,0 ); glVertex3f(-1,1,0); 
+    glColor3fv(&m_E.x);
+    glVertex3f(1, 1, 0);
+    glVertex3f(1, -1, 0);
+    glVertex3f(-1, -1, 0);
+    glVertex3f(1, 1, 0);
+    glVertex3f(-1, -1, 0);
+    glVertex3f(-1, 1, 0);
     glEnd();
 }
 
-void AreaLight::sample(float& pdf, Vec3f& p, int base, Random& rnd) {
-    // YOUR CODE HERE (R1): Integrate your area light implementation.
+void AreaLight::sample(float& pdf, Vec3f& p, int index, Random& rnd) {
+    // YOUR CODE HERE (R2):
+    // You should draw a random point on the light source and evaluate the PDF.
+    // Store the results in "pdf" and "p".
+    //
+    // Note: The "size" member is _one half_ the diagonal of the light source.
+    // That is, when you map the square [-1,1]^2 through the scaling matrix
+    //
+    // S = ( size.x    0   )
+    //     (   0    size.y )
+    //
+    // the result is the desired light source quad (see draw() function above).
+    // This means the total area of the light source is 4*size.x*size.y.
+    // This has implications for the computation of the PDF.
 
-    // (this does not do what it's supposed to!)
-    pdf = 1.0f;
-    p = Vec4f(m_xform.getCol(3)).getXYZ();
+    // For extra credit, implement QMC sampling using some suitable sequence.
+    // Use the "index" input for controlling the progression of the sequence from
+    // the outside. If you only implement purely random sampling, "index" is not required.
+
+    pdf = 1.f / (4.f * m_size.x * m_size.y);
+
+    auto x = rnd.getF32(-1.f, 1.f);
+    auto y = rnd.getF32(-1.f, 1.f);
+
+    // auto x = sobol::sample(index, 0) * 2 - 1;
+    // auto y = sobol::sample(index, 1) * 2 - 1;
+
+    Mat4f S = Mat4f::scale(Vec3f(m_size, 1));
+    p = m_xform * S * Vec3f(x, y, 0.f);
 }
 
+void AreaLight::sampleStratified(float& pdf,
+                                 Vec3f& p,
+                                 int index,
+                                 Random& rnd,
+                                 float x,
+                                 float y,
+                                 float halfStepX,
+                                 float halfStepY) {
+    pdf = 1.f / (4.f * m_size.x * m_size.y);
+    auto jitterX = rnd.getF32(-halfStepX, halfStepX);
+    auto jitterY = rnd.getF32(-halfStepY, halfStepY);
+    Mat4f S = Mat4f::scale(Vec3f(m_size, 1));
+    p = m_xform * S * Vec3f(x + jitterX, y + jitterY, 0.f);
+}
 
-} // namespace FW
+}  // namespace FW
--- assignment4/src/base/rtutil.hpp
+++ assignment4/src/base/rtutil.hpp
@@ -1,31 +1,29 @@
 #pragma once
 
 /*
- * utilities for internal use etc. in the raytracer class and the bvh construction
+ * utilities for internal use etc. in the raytracer class and the bvh
+ * construction
  */
+
+#include <iostream>
+#include <limits>
+#include <tuple>
 
 #include "base/Math.hpp"
 
-#include <iostream>
-
-
 namespace FW {
 
-
 enum SplitMode {
-	SplitMode_SpatialMedian,
-	SplitMode_ObjectMedian,
-	SplitMode_Sah,
-	SplitMode_None,
-	SplitMode_Linear
+    SplitMode_SpatialMedian,
+    SplitMode_ObjectMedian,
+    SplitMode_Sah,
+    SplitMode_None,
+    SplitMode_Linear
 };
 
 struct Plane : public Vec4f {
-    inline float dot(const Vec3f& p) const {
-        return p.x * x + p.y * y + p.z * z + w;
-    }
+    inline float dot(const Vec3f& p) const { return p.x * x + p.y * y + p.z * z + w; }
 };
-
 
 struct AABB {
     Vec3f min, max;
@@ -34,9 +32,32 @@ struct AABB {
     inline F32 area() const {
         Vec3f d(max - min);
         return 2 * (d.x * d.y + d.x * d.z + d.y * d.z);
+    };
+    void unionWith(const RTTriangle& triangle) {
+        min = FW::min(min, triangle.min());
+        max = FW::max(max, triangle.max());
+    };
+    std::tuple<bool, float> intersect(const Vec3f& orig,
+                                      const Vec3f& dir,
+                                      const Vec3f& invDir,
+                                      const float tmin) const {
+        float tstart = -std::numeric_limits<float>::max(), tend = std::numeric_limits<float>::max();
+        float t1, t2;
+        for (size_t i = 0; i < 3; i++) {
+            if (dir.get(i) == 0.0) {
+                if (orig.get(i) < min.get(i) || orig.get(i) > max.get(i))
+                    return std::make_tuple(false, 0.0f);
+            } else {
+                t1 = (min.get(i) - orig.get(i)) * invDir.get(i);
+                t2 = (max.get(i) - orig.get(i)) * invDir.get(i);
+                if (t1 > t2) std::swap(t1, t2);
+                tstart = FW::max(tstart, t1);
+                tend = FW::min(tend, t2);
+            }
+        }
+        return std::make_tuple(!(tstart > tend || tend < tmin), tstart);
     }
 };
-
 
 inline std::ostream& operator<<(std::ostream& os, const FW::Vec3f& v) {
     return os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
@@ -50,5 +71,4 @@ inline std::ostream& operator<<(std::ostream& os, const AABB& bb) {
     return os << "BB(" << bb.min << ", " << bb.max << ")";
 }
 
-
-}
+}  // namespace FW
